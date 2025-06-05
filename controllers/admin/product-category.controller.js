ef3c19171a133471e0a2decbe82da532356148b0
Tran Cong Duc <trancongduc0812@gmail.com>
2025-06-05 15:58:02 +0700

    fix: category

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -1,4 +1,3 @@
-const filterStatusHelper = require("../../helpers/filterStatus");
 const Category = require("../../models/category.model");
 const systemConfig = require("../../config/system");
 
@@ -7,12 +6,26 @@ module.exports.index = async (req, res) => {
   let find = {
     deleted: false,
   };
+  function createTree(arr, parentId = "") {
+    const tree = [];
+    arr.forEach((item) => {
+      if (item.parent_id === parentId) {
+        const newItem = item;
+        const children = createTree(arr, item.id);
+        if (children.length > 0) {
+          newItem.children = children;
+        }
+        tree.push(newItem);
+      }
+    });
+    return tree;
+  }
   const records = await Category.find(find);
+  const newRecords = createTree(records);
 
-  const filterStatus = filterStatusHelper(req.query);
   res.render("admin/pages/category/index", {
     pageTitle: "Danh mục sản phẩm",
-    records: records,
+    records: newRecords,
   });
 };
 
@@ -39,8 +52,6 @@ module.exports.create = async (req, res) => {
 
   const records = await Category.find(find);
   const newRecords = createTree(records);
-
-  const filterStatus = filterStatusHelper(req.query);
   res.render("admin/pages/category/create", {
     pageTitle: "Thêm danh mục sản phẩm",
     records: newRecords,
