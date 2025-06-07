fde5e21256e74cd807fdca081d79245094bfef50
Tran Cong Duc <trancongduc0812@gmail.com>
2025-06-07 17:10:48 +0700

    xay dung danh sach phan quyen

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -3,6 +3,8 @@ const systemConfig = require("../../config/system")
 const dashboardRoutes = require("./dashboard.route")
 const productRoutes = require("./product.route")
 const categoryRoutes = require("./category.route");
+const roleRoutes = require("./roles.route");
+
 
 
 module.exports = (app) => {
@@ -10,4 +12,5 @@ module.exports = (app) => {
   app.use(PATH_ADMIN + "/dashboard", dashboardRoutes);
   app.use(PATH_ADMIN + "/product", productRoutes);
   app.use(PATH_ADMIN + "/category", categoryRoutes);
+  app.use(PATH_ADMIN + "/roles", roleRoutes);
 };
