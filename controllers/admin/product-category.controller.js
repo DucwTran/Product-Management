98e935d356b34d5ce2460dd01fd79a412df4fd55
Tran Cong Duc <trancongduc0812@gmail.com>
2025-06-07 15:33:58 +0700

    chinh sua category

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -29,7 +29,7 @@ module.exports.index = async (req, res) => {
   });
 };
 
-// [GET] /admin/category
+// [GET] /admin/category/create
 module.exports.create = async (req, res) => {
   let find = {
     deleted: false,
@@ -71,4 +71,55 @@ module.exports.createPost = async (req, res) => {
   await record.save();
 
   res.redirect(`${systemConfig.prefixAdmin}/category`);
+};
+
+// [GET] /admin/category/edit/:id
+module.exports.edit = async (req, res) => {
+  const id = req.params.id;
+  try {
+    let find = {
+      deleted: false,
+    };
+
+    function createTree(arr, parentId = "") {
+      const tree = [];
+      arr.forEach((item) => {
+        if (item.parent_id === parentId) {
+          const newItem = item;
+          const children = createTree(arr, item.id);
+          if (children.length > 0) {
+            newItem.children = children;
+          }
+          tree.push(newItem);
+        }
+      });
+      return tree;
+    }
+
+    const data = await Category.findOne({
+      _id: id,
+      deleted: false,
+    });
+
+    const records = await Category.find(find);
+    const newRecords = createTree(records);
+
+    res.render("admin/pages/category/edit", {
+      pageTitle: "Chỉnh sửa danh mục sản phẩm",
+      data: data,
+      records: newRecords,
+    });
+  } catch (error) {
+    res.redirect(`${systemConfig.prefixAdmin}/category`);
+  }
+};
+
+// [PATCH] /admin/category/edit/:id
+module.exports.editPatch = async (req, res) => {
+  const id = req.params.id;
+  req.body.position = parseInt(req.body.position);
+
+  await Category.updateOne({ _id: id }, req.body);
+
+  res.redirect("back");
 };
